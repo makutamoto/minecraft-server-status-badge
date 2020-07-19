32a9af36b36733640083db83259558f1d1494ed8
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-07-20 00:23:01 +0900

    + version

--- pages/api/server/[host].ts
+++ pages/api/server/[host].ts
@@ -7,9 +7,9 @@ export interface Online {
     online: number,
 }
 
-const json = (host: string, online: Online | null) => ({
+const json = (version: string, online: Online | null) => ({
     schemaVersion: 1,
-    label: host,
+    label: `Minecraft ${version}`,
     message: online === null ? 'Offline' : `${online.online}/${online.max}`,
     color: online === null ? 'red' : 'green',
     cacheSeconds: 1800,
@@ -19,6 +19,7 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
     let host = req.query.host as string;
     let status = await fetchStatus(-1, host, 25565);
     let online: Online = status && status.players;
+    let version = status && status.version.name;
     res.setHeader('Content-type', 'application/json');
-    res.status(200).send(JSON.stringify(json(host, online)));
+    res.status(200).send(JSON.stringify(json(version, online)));
 }
